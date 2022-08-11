101514d17860bd9cd1ff2b3202ab590ac3af4780
sotengboy <sotengboy@gmail.com>
2022-08-12 01:12:49 +0700

    CoreUI Commit

---
+++ src/views/pages/posts/Posts.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import { DocsExample } from 'src/components'
+
+const Posts = () => {
+  return (
+    <CRow>
+      <CCol xs={12}>
+        <CCard className="mb-4">
+          <CCardHeader>
+            <strong>Posts Data</strong>
+          </CCardHeader>
+          <CCardBody>
+            <p className="text-medium-emphasis small">List of Created Posts</p>
+            <DocsExample href="components/table">
+              <CTable>
+                <CTableHead>
+                  <CTableRow>
+                    <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Class</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Heading</CTableHeaderCell>
+                  </CTableRow>
+                </CTableHead>
+                <CTableBody>
+                  <CTableRow>
+                    <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                    <CTableDataCell>Mark</CTableDataCell>
+                    <CTableDataCell>Otto</CTableDataCell>
+                    <CTableDataCell>@mdo</CTableDataCell>
+                  </CTableRow>
+                </CTableBody>
+              </CTable>
+            </DocsExample>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  )
+}
+
+export default Posts
